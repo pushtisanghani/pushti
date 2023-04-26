18628c4f2b32dfe91eb9edd7eded12e622d13af2
pushtisanghani <131763039+pushtisanghani@users.noreply.github.com>
2023-04-26 15:24:27 +0530

    lab work

---
+++ 1.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    clrscr();
+    int a,b;
+    printf("enter value a=");
+    scanf("%d",&a);
+    printf("enter value b=");
+    scanf("%d",&b);
+
+    printf("addition of a/b=%d",a/b);
+    getch();
+    }
